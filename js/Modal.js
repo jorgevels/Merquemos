f0d7e14e55a0bf6c002d6a3bb7c5259900c6bff6
jorgevels <jorgevelasquez006@hotmail.com>
2019-10-16 10:24:49 -0500

    Agregando un SW

---
+++ js/Modal.js
@@ -0,0 +1,16 @@
+const $instructions = document.getElementById("instrucciones");
+$instructions.addEventListener("click", () => {
+  Swal.fire({
+    type: "info",
+    title: "Merquemos",
+    html: `<div style="text-align: left;"><strong>1.-</strong>No mas listas de compras en papel.</div>
+      <div style="text-align: left;"><strong>2.-</strong>Con esta PWA podras crear tus listas de compras.</div>
+                 <div style="text-align: left;"><strong>3.-</strong>Con el boton del signo mas de color verde, podras agregar tus articulos a una lista muy cool.</div>
+                 <div style="text-align: left;"><strong>4.-</strong>Puedes seleccionar la cantidad y la unidad que desees.</div>
+                 <div style="text-align: left;"><strong>5.-</strong>Si quieres eliminar un articulo de la lista, con el boton del signo menos de color rojo, lo podras hacer.</div>
+                 <div style="text-align: left;"><strong>6.-</strong>Al momento de tus compras, en la columna de seleccion podras ir marcando los productos que ya depositaste en tu carro de compras .</div>
+                 <br>
+                 <div style="text-align: right;"><strong>Dev.</strong> Jorge Velasquez 😉</div>
+                 `
+  });
+});
